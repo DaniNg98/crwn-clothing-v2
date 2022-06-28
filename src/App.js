a1bb802ae5fa70c1828f7e4e6d375f5d6d291b9d
Daniel Nguyen <danielng@ulrik.uio.no>
2022-06-28 12:40:36 +0200

    Added logo and styles to nagivation component. Fixed Typo in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Navigation from './routes/navigation/navigation.component';
-
 import Home from './routes/home/home.component';
 
 const Shop = () => {
@@ -12,8 +11,8 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
-        <Route index path='/' element={<Home />} />
-        <Route path='shop' elememt={<Shop />} />
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
   );
